8887342708e856d3ee789b89454d719e7ed8ac58
Andre Marcio <andre.marcio.ti@gmail.com>
2026-05-10 19:22:45 -0300

    verify task

--- app/main.py
+++ app/main.py
@@ -10,9 +10,9 @@ def get_human_age(cat_age: int, dog_age: int) -> list:
             return 0 if cat_age < 15 else 1
 
     def dog_age_to_human(dog_age: int) -> int:
-        
-        if dog_age > 24:
-            return ((dog_age - 24) // 5) + 2
+
+        if dog_age > 29:
+            return ((dog_age - 29) // 5) + 2
         elif dog_age == 24:
             return 2
         else:
